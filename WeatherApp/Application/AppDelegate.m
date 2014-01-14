131ffad181eb061ec3836e54c58b873cecb9b5d3
Renzo Crisóstomo <ruenzuo@gmail.com>
2014-01-14 12:36:22 -0500

    Adding MainStoryboard and CitiesViewController

--- WeatherApp/Application/AppDelegate.m
+++ WeatherApp/Application/AppDelegate.m
@@ -10,11 +10,9 @@
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+-           (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
---
+++ WeatherApp/Controllers/CitiesViewController.h
@@ -0,0 +1,15 @@
+//
+//  CitiesViewController.h
+//  WeatherApp
+//
+//  Created by Renzo Crisóstomo on 1/14/14.
+//  Copyright (c) 2014 Ruenzuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CitiesViewController : UITableViewController
+
+- (IBAction)onRefreshControlValueChanged:(id)sender;
+
+@end
